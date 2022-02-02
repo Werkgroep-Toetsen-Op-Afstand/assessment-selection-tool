6704a2ad73d97f43fc42311d9820c662002a9291
Sharon Klinkenberg <shklinkenberg@Sharons-MacBook-Pro.local>
2022-02-02 17:57:28 +0100

    added first version for script to create word docs based on multiple md/Rmd files

---
+++ CreateDocx.R
@@ -0,0 +1,28 @@
+docs.all <- list.files("content/1-on-1/application/", pattern = "(\\.md|\\.Rmd)", recursive = T)
+
+docs.nl <- grep("en", docs.all, invert = T, value = T)
+docs.nl
+
+
+yaml = "---
+title: test
+---\n"
+
+chunks <- paste0("```{r, warning=FALSE, echo=FALSE, results='asis'}\n",
+                 "if(!berryFunctions::is.error(yaml::read_yaml('", paste0("content/1-on-1/application/",docs.nl), "')) ) {\n",
+                 "paste(",
+                 "yaml::read_yaml('", paste0("content/1-on-1/application/",docs.nl), "')$title,\n", 
+                 "yaml::read_yaml('", paste0("content/1-on-1/application/",docs.nl), "')$subtitle) -> title\n
+cat(paste('##', title))\n}\n```\n \n",
+                 "```{r child = '", paste0("content/1-on-1/application/",docs.nl), "'}\n```\n \n",
+                 "---\n")
+
+
+sink("temp.Rmd")
+cat(yaml, sep = '\n' )
+cat(chunks, sep = '\n')
+sink()
+
+bookdown::render_book(input         = "temp.Rmd", 
+                      output_format = "word_document",
+                      output_file   = "OneOnOneNL.docx")
